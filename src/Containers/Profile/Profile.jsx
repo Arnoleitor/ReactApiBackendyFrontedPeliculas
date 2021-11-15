bf6ae49f32c023f3f10493104ec365f754526570
Arnoleitor <arnold201991@gmail.com>
2021-11-15 13:55:51 +0100

    actualización ya funcional,mejoras de diseño

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -4,15 +4,12 @@ import { connect } from 'react-redux';
 import './Profile.css';
 import { LOGOUT, UPDATE_USER } from '../../redux/types';
 import axios from 'axios';
-import { getDefaultNormalizer } from '@testing-library/dom';
 
 
 
 const Profile = (props) => {
 
-    let config = {
-        headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Im5vbWJyZSI6InBvbGxvIiwiZW1haWwiOiJwb2xsb0BnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQ3WFBEMWZiZnRWbjhka3lma0RlTDF1MW13dVlSeUIzdjRQWnJzSzNtUno1Y3lOaXdRUkMzTyIsInJvbCI6ImFkbWluIiwiX2lkIjoiNjE4ZTViNTI5YjYwODY3OGEwYWRiNTU2IiwiY3JlYXRlZEF0IjoiMjAyMS0xMS0xMlQxMjoxNzoyMi43ODdaIiwidXBkYXRlZEF0IjoiMjAyMS0xMS0xMlQxMjoxNzoyMi43ODdaIiwiX192IjowfSwiaWF0IjoxNjM2NzE5NDQyLCJleHAiOjE2MzY5MzU0NDJ9.myGBiHAj8i-4s2OmLLeBXqius0xlQY4YZYiDqWbcwWU" }
-    };
+    
 
     const [msgError, setmsgError] = useState("");
     const [userData, setUserData] = useState(props.credentials.user);
@@ -25,30 +22,17 @@ const Profile = (props) => {
     const update = async () => {
         props.dispatch({ type: UPDATE_USER, payload: userData });
         console.log("aqui llego")
-        let body = {
-            _id: "618e5b529b608678a0adb556",
-            nombre: "pollo123",
-            email: "pollo@gmail.com",
-            contrasena: "12345678",
-            rol: "admin",
-            createdAt: "2021-11-02T16:09:46.538Z",
-            updatedAt: "2021-11-02T16:09:46.538Z",
-            __v: 0
+        
+        let token = {
+            headers: { Authorization: `Bearer ${props.credentials.token}` }
         };
-
         
         try {
-            console.log(body)
 
-            let res = await axios.put("https://proyectopeliculasgeekshubs.herokuapp.com/usuario/update/618bfdc171d2c27899afb369", body,config);
+            let res = await axios.put(`https://proyectopeliculasgeekshubs.herokuapp.com/usuario/${userData._id}`,userData, token);
             setmsgError(`Datos actualizados ${res.data.user.body}....`);
-            console.log("respuesta", res);
-            localStorage.setItem("datosLogin", JSON.stringify(res.data.user));
-
-            let datos = res.data;
-
-            props.dispatch({ type: UPDATE_USER, payload: datos });
-            console.log(datos)
+            
+            //redireccionar a otra O MOSTRAR MENSAJE DE ACTUALIZACIÓN
 
         } catch (error) {
             setmsgError("Fallo al actualizar datos");
@@ -75,15 +59,26 @@ const Profile = (props) => {
     if (props.credentials?.token !== '') {
         return (
             <div className="designProfile">
-                <pre>{JSON.stringify(userData, null, 2)}</pre>
-                <div className="user"><input value={userData?.nombre || ""} name="nombre" onChange={manejaInputs} /></div>
+                <div className="cuadrado1">
+                    <div className="user"><input id="nombre" value value={userData?.nombre || ""} name="nombre" onChange={manejaInputs} /></div>
+                    <div className="user"><input id="email" value={userData?.email || ""} name="email" onChange={manejaInputs} /></div>
+                    <div className="update" onClick={() => update()}>ACTUALIZAR</div>
+
+                </div>
+                <div className="recuadroperfilInfo">
+                    gnhty
+                    dddd
+                <div id="logout" onClick={() => logOut()}>LOGOUT</div>
+                </div>
+                {/* <pre>{JSON.stringify(userData, null, 2)}</pre> */}
+                
                 {/* <div className="user">{props.credentials?.user?.apellidos}</div>
                 <div className="user">{props.credentials?.user?.ciudad}</div> */}
-                <div className="user"><input value={userData?.email || ""} name="email" onChange={manejaInputs} /></div>
+                
                 {/* <div className="user">{props.credentials?.user?.telefono}</div>
                 <div className="user">{props.credentials?.user?.direccion}</div> */}
-                <div className="update" onClick={() => update()}>UPDATE</div>
-                <div id="logout" onClick={() => logOut()}>LOGOUT</div>
+                
+                
             </div>
 
 
