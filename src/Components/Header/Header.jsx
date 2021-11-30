1296f781253f49a81db1d716e7df845c9edddd2a
Arnold <arnold201991@gmail.com>
2021-11-30 19:53:40 +0100

    implementada info de persona que esta conectada ,en el header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css';
 import Boton from '../Boton/Boton';
 import logo from '../../assets/img/netflix.png';
 import { connect } from 'react-redux';
+import profile from '../../assets/img/usuarioicono.png';
 
 
 
@@ -12,8 +13,9 @@ const Header = (props) => {
     const llevame = () => {
         history("/");
     }
-
+    console.log(props.credentials.user.nombre)
     return(
+    
         <div className="designHeader">
             <div>
                 <img id="logo" src={logo} alt="logo" onClick={()=>llevame()} />
@@ -24,8 +26,12 @@ const Header = (props) => {
                 <Boton destino="Registro" url="/register"/>
                 <Boton destino="Login" url="/login"/>
                 <Boton destino="Peliculas" url="/Peliculas"/>
-                <div id="admin">{props.credentials?.user.rol=='admin' && <Boton destino="Admin" url="/admin"/>}</div> 
-                
+                <div id="admin">{props.credentials?.user.rol=='admin' && <Boton destino="Admin" url="/admin"/>}</div>
+                <div className="icono-user-logeado">
+                <img className="img-profile" src={profile} alt="profile"  />
+                    {props?.credentials?.user?.nombre}
+                    
+                </div>
             </div>
         </div>
     )
